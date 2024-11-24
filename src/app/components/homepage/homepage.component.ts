dfe813e581913696d0420ffefa714924261e0024
mbvgua <mbuguadouglas17@gmail.com>
2024-11-24 13:22:01 +0300

    Routing within the app
    Built all the needed pages with the help of tailwind css, and integrated
    basic routing into the application with the help of angulars inbuilt
    routerModule()
    
    Responsivenes was also taken into account while creating the components

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { FaqsComponent } from "../faqs/faqs.component";
+import { AboutUsComponent } from "../about-us/about-us.component";
+import { OurServicesComponent } from "../our-services/our-services.component";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [FooterComponent, FaqsComponent],
+  imports: [FooterComponent, FaqsComponent, AboutUsComponent, OurServicesComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
