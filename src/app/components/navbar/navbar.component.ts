f677805acf443ff06f5234223254ba6baefce0d6
mbvgua <mbuguadouglas17@gmail.com>
2024-12-02 17:05:32 +0300

    make the navbar sticky on scroll and also add the faqs accordingly

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -12,13 +12,25 @@ export class NavbarComponent {
 
   @ViewChild('hamburgerMenu', { static: true }) hamburgerMenu!: ElementRef;
   @ViewChild('mobileMenu', { static: true }) mobileMenu!: ElementRef;
+  @ViewChild('navbar', { static: true }) navbar!: ElementRef;
 
   ngOnInit() {
     this.hamburgerMenu.nativeElement.addEventListener('click', this.toggleMenu.bind(this));
+
+    /**   trid to make the navbar transparent at first, then emerge on scroll
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        this.navbar.nativeElement.classList.add('bg-sky-600', 'shadow-lg'); // Adds dark background and shadow on scroll
+      } else {
+        this.navbar.nativeElement.classList.remove('bg-transparent','shadow-lg'); // Reverts to transparent when at top
+      }
+    });
+    */
   }
 
   toggleMenu() {
     const menu = this.mobileMenu.nativeElement;
     menu.classList.toggle('hidden');
   }
+    
 }
