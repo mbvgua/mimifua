dbdfaff5a9f667b4ce6f9b1652581825e90cc5ac
mbvgua <mbuguadouglas17@gmail.com>
2024-12-10 13:56:00 +0300

    using wow.js, incorporate animations into the website. However realized that the package is deprecated and there is now a preference for AOS(animate on scroll) will learn more on it use and incorporation later

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { FaqsComponent } from "../faqs/faqs.component";
 import { RouterModule } from '@angular/router';
+// import {SwiperModule} from 'swiper/angular'
+// import { Navigation, Pagination } from 'swiper/modules';
 
 @Component({
   selector: 'app-homepage',
@@ -10,10 +12,49 @@ import { RouterModule } from '@angular/router';
     FooterComponent,
     FaqsComponent,
     RouterModule,
-],
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+  constructor() {}
+
+  @ViewChild('testimonialCarousel', { static: true }) testimonialCarousel!: HTMLDivElement;
+  @ViewChild('nextButton', { static: true }) nextButton!: HTMLDivElement;
+  @ViewChild('previousButton', { static: true }) previousButton!: HTMLDivElement;
+
+
+  ngOnInit(): void {
+    
+    // // Testimonial
+    // const testimonialSwiper = new Swiper(this.testimonialCarousel, {
+    //   slidesPerView: 1,
+    //   spaceBetween: 30,
+    
+    //   // Navigation arrows
+    //   navigation: {
+    //     nextEl: this.nextButton,
+    //     prevEl: this.previousButton,
+    //   },
+    
+    //   breakpoints: {
+    //     640: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 30,
+    //     },
+    //     1024: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 30,
+    //     },
+    //     1280: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 30,
+    //     },
+    //   },
+    // });
+    
+
+    
+  }
 
 }
