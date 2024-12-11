362a1ad6de61b255226d61b4cd4810163df9ee9e
mbvgua <mbuguadouglas17@gmail.com>
2024-12-11 22:04:11 +0300

    add the vercel speed analytics settings

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
 inject();
+injectSpeedInsights();
