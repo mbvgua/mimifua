674d53a32744ff9b87dc7b7aed47d46605bd148d
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-12-21 00:18:27 +0300

    Integrate the signin form functionality

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { RouterLink } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
   imports: [
     FooterComponent,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
@@ -19,10 +22,27 @@ export class SigninComponent implements OnInit{
 
   ngOnInit(): void {
     this.signInForm = new FormGroup({
-      useraname: new FormControl(''),
-      email:new FormControl(''),
-      password: new FormControl('')
+      userNameOrEmail: new FormControl(null,
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ],
+      ),
+      password: new FormControl(null,
+        [
+          Validators.required,
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,10}$'),
+        ],
+      ),
+      rememberMe: new FormControl(null, )
     })
+  }
+
+  // on form submition
+  onSubmit(){
+    console.log(this.signInForm.value)
+    this.signInForm.reset()
   }
 
 
